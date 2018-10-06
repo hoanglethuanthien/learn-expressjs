4ef9c2dd39ec4657580767a9bd378f6c0a246f61
hoanglethuanthien <hoanglethuanthien@gmail.com>
2018-10-06 08:07:42 +0700

    fixed conflict

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 let port = 3000;
@@ -37,43 +36,3 @@ app.listen(3000, function () {
 }) 
 
 
-=======
-const express = require('express');
-const app = express();
-let port = 3000;
-
-let users = [
-		{ id: 1, name: 'Thien'},
-		{ id: 2, name: 'Hong'},
-		]
-app.set('view engine', 'pug');
-app.set('views', './views');
-
-app.get('/', function(req, res) {
-	res.render('index', {
-		name: 'THien'
-	});
-});
-
-app.get('/users', function(req, res) {
-	res.render('users/index', {
-		users: users,
-	});
-})
-
-app.get('/users/search', function(req, res){
-	let q = req.query.q;
-	let matchedUsers = users.filter(function(user){
-		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-	});
-	res.render('users/index', {
-		users: matchedUsers,
-	});
-
-})
-app.listen(3000, function () {
-	console.log('Example app listening on port ' + port);
-}) 
-
-
->>>>>>> d63eef32d93a20f6dc0ad2f64f32635bbc01b008
