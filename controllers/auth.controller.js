0c53a623de9272bc2c792695361d750e52ef7c55
hoanglethuanthien <hoanglethuanthien@gmail.com>
2018-10-20 17:26:41 +0700

    postLogin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,3 +3,29 @@ var db = require('../db');
 module.exports.login = function (req, res) {
     res.render('auth/login');
 }
+
+module.exports.postLogin = function (req, res){
+    var email = req.body.email;
+    var password = req.body.password;
+    var user = db.get('users').find({email: email}).value();
+
+    if(!user) {
+        res.render('auth/login', {
+            errs: [
+                'User does not exist.'
+            ],
+            values: req.body,
+        });
+        return;
+    }
+    if(user.password !== password){
+        res.render('auth/login', {
+            errs: [
+                'Wrong password.'
+            ],
+            values: req.body,
+        });
+        return;
+    }
+    res.redirect('/users');
+}
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,4 +4,6 @@ var controller = require('../controllers/auth.controller')
 
 router.get('/login', controller.login);
 
+router.post('/login', controller.postLogin);
+
 module.exports = router;
