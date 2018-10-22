11b32739db5b79333cfdcf96472f48d3f7a1d9a9
hoanglethuanthien <hoanglethuanthien@gmail.com>
2018-10-22 13:55:13 +0700

    md5 hashedpassword

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,4 @@
+var md5 = require('md5');
 var db = require('../db');
 
 module.exports.login = function (req, res) {
@@ -18,7 +19,9 @@ module.exports.postLogin = function (req, res){
         });
         return;
     }
-    if(user.password !== password){
+
+    var hashedPassword = md5(password);
+    if(user.password !== hashedPassword){
         res.render('auth/login', {
             errs: [
                 'Wrong password.'
