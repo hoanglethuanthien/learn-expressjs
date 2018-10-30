c597b9c6869392e17be7b09af3f8703b57d13852
hoanglethuanthien <hoanglethuanthien@gmail.com>
2018-10-30 17:39:35 +0700

    update view product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,7 +2,7 @@ var db = require('../db');
 
 module.exports.index = function(req, res){
 
-    var products = db.get('products').value();
+    var products = db.get('products').value().slice(0,8) ;
     res.render('products', {
         products: products,
     })
