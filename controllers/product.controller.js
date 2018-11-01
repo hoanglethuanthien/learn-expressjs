bbe1029de5529ec95b2cfbdcb7483fb5a28976f4
hoanglethuanthien <hoanglethuanthien@gmail.com>
2018-11-01 21:57:42 +0700

    pageination, search products

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,9 +1,23 @@
 var db = require('../db');
 
 module.exports.index = function(req, res){
-
-    var products = db.get('products').value().slice(0,8) ;
+    var page = req.query.page || 1 ;
+    var perPage = 8;
+    var start = (page-1)* perPage;
+    var end = page* perPage;
+    var products = db.get('products').value().slice(start, end) ;
     res.render('products', {
         products: products,
     })
+}
+
+module.exports.search= function (req, res) {
+	let q = req.query.q;
+	let products = db.get('products').value();
+	let matchedProducts = products.filter(function (product) {
+		return product.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+	res.render('products/index', {
+		products: matchedProducts,
+	});
 }
