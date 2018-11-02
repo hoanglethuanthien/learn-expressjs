30490e412f20f4a3dd6bdf5fd5c3a8b6781bc61a
hoanglethuanthien <hoanglethuanthien@gmail.com>
2018-11-02 09:41:33 +0700

    save controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,8 +1,12 @@
 var db = require('../db');
 
 module.exports.index = function(req, res){
+    var page = parseInt(req.query.page) ||1; //n
+    var perPage = 8; //x
 
-    var products = db.get('products').value().slice(0,8) ;
+    var start = (page -1)* perPage;
+    var end = page * perPage;
+    var products = db.get('products').value().slice(start,end) ;
     res.render('products', {
         products: products,
     })
