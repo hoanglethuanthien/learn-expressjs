90d8b9a244ac68486b4ee9533089fa389d33d7b8
hoanglethuanthien <hoanglethuanthien@gmail.com>
2018-11-03 16:33:36 +0700

    sessionId

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,19 +1,27 @@
 var db = require('../db')
 
+module.exports.index = function(req, res, next){
+    var sessionId = req.signedCookies.sessionId;
+    var cart = db.get('sessions').find({id: sessionId}).get('cart').value();
+                
+    res.render('cart', {
+        cart: cart
+    })
+}
 
 module.exports.addToCart = function(req, res, next){
-    var productId = req.param.productId;
+    var productId = req.params.productId;
     var sessionId = req.signedCookies.sessionId;
-
+    
     if(!sessionId){
         res.redirect('/products');
         return;
     }
-
+    var count = db.get('sessions').find({id:sessionId}).get('cart.' + productId, 0).value();
     db.get('sessions')
         .find({id: sessionId})
-        .set('cart.'+ productId, 1)
-        .write();
+        .set('cart.'+ productId, count + 1)
+        .write(); 
 
     res.redirect('/products');
 } ;
